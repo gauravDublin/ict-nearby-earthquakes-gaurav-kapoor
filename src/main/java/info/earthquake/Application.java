97adb58474ce5aea46ffb0de4aaf4f9f66f14359
Gaurav Kapoor <gaurav.kapoor@tipico.com>
2019-06-27 09:31:17 +0200

    8: removed extra instanttiation of main class

--- src/main/java/info/earthquake/Application.java
+++ src/main/java/info/earthquake/Application.java
@@ -2,7 +2,6 @@ package info.earthquake;
 
 import info.earthquake.service.USGCService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -14,7 +13,6 @@ public class Application {
 	private static USGCService service;
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
 		AbstractApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 		context.getBean(USGCService.class).calculateNearestEarthquakes();
 	}
